251b2e96f0b24a9a087e8b94fc2bc7c314e3ea31
Stephanie Peacock <stephanie.j.peacock@gmail.com>
2023-07-20 17:28:25 -0700

    Creating of CU-summary table for Eric

--- tables/code/CU-summary-table.R
+++ tables/code/CU-summary-table.R
@@ -41,17 +41,16 @@ var_to_keep <- c("region",
 								 "species_name", 
 								 "cuid", 
 								 "cu_name_pse", 
+								 "exp",
 								 "curr_spw", 
 								 "curr_spw_start_year", 
 								 "curr_spw_end_year",
+								 "cu_enh_rank",
 								 "psf_status",
 								 "cosewic_status",
 								 "cosewic_status_yr",
 								 "wsp_status",
 								 "wsp_status_yr",
-								 "extinct",
-								 "total_exploitation_rate",
-								 "total_exploitation_rate_yr",
 								 "rollup_hi_pct",
 								 "rollup_mod_pct",
 								 "rollup_lo_pct"
@@ -70,7 +69,10 @@ dat102 <- dbGetQuery(
 )
 dat102[which(dat102 == -989898, arr.ind = TRUE)] <- NA
 
-full_join(app4, dat102)[, c("reg")]
+cu_summaries_prelim1 <- full_join(app4, dat102)
+cu_summaries_prelim2 <- cu_summaries_prelim1[, names(cu_summaries_prelim1) %in% var_to_keep]
+
+
 # Get freshwater habitat pressure
 app13 <- dbGetQuery(
 	conn = connec, 
@@ -82,40 +84,45 @@ regions <- data.frame(
 	region_name = c("Skeena", "Nass", "Central Coast", "Fraser", "Vancouver Island & Mainland Inlets", "Haida Gwaii", "Columbia", "Yukon", NA, "Transboundary")
 )
 app13$region <- regions$region_name[app13$regionid]
+# Remove species name because formatted differently from other tables and so it duplicates on full join
+app13 <- app13[, which(names(app13) != "species_name")]
 
-# Read catch and run size
-exploitation <- dbGetQuery(
-	conn = connec, 
-	statement = "SELECT * FROM appdata.vwdl_dataset4_output"
-)
-exploitation[which(exploitation == -989898, arr.ind = TRUE)] <- NA
+cu_summaries_prelim3 <- full_join(app13, cu_summaries_prelim2)
+cu_summaries_prelim4 <- cu_summaries_prelim3[, names(cu_summaries_prelim3) %in% var_to_keep]
 
-dat102vs4 <- data.frame(cuid = dat102$cuid, dat102 = NA, dat4 = NA)
-for(i in 1:nrow(dat102vs4)){
-	dat102vs4$dat102[which(dat102vs4$cuid == dat102vs4$cuid[i])] <-  dat102$exp[which(dat102$cuid == dat102vs4$cuid[i])]
-	dum1 <- exploitation[which(exploitation$cuid == dat102vs4$cuid[i]), c('total_exploitation_rate', 'year')]
-	dum1 <- dum1[!is.na(dum1$total_exploitation_rate),]
-	if(nrow(dum1) > 0){
-		
-	dat102vs4$dat4[which(dat102vs4$cuid == dat102vs4$cuid[i])] <- dum1$total_exploitation_rate[which(dum1$year == max(dum1$year))]
-	}
-	rm(dum1)
 
-}
+# # Read catch and run size
+# exploitation <- dbGetQuery(
+# 	conn = connec, 
+# 	statement = "SELECT * FROM appdata.vwdl_dataset4_output"
+# )
+# exploitation[which(exploitation == -989898, arr.ind = TRUE)] <- NA
+# 
+# dat102vs4 <- data.frame(cuid = dat102$cuid, dat102 = NA, dat4 = NA)
+# for(i in 1:nrow(dat102vs4)){
+# 	dat102vs4$dat102[which(dat102vs4$cuid == dat102vs4$cuid[i])] <-  dat102$exp[which(dat102$cuid == dat102vs4$cuid[i])]
+# 	dum1 <- exploitation[which(exploitation$cuid == dat102vs4$cuid[i]), c('total_exploitation_rate', 'year')]
+# 	dum1 <- dum1[!is.na(dum1$total_exploitation_rate),]
+# 	if(nrow(dum1) > 0){
+# 		
+# 	dat102vs4$dat4[which(dat102vs4$cuid == dat102vs4$cuid[i])] <- dum1$total_exploitation_rate[which(dum1$year == max(dum1$year))]
+# 	}
+# 	rm(dum1)
+# 
+# }
 
-############################################
-# Make monster table
-###############################################################################
+# Join hatchery release scores
+hatch <- read.csv("tables/code/ignore/CU_enhancement_levelFeb142023.csv")
+head(hatch)
+head(cu_summaries_prelim4)
 
-# relationship = "one-to-one" means an error willbe thrown if cuid is duplicated
-# in either table
-
-CUsummary <- full_join(app4, app13)
-	
+cu_summaries_prelim5 <- full_join(hatch, cu_summaries_prelim4)
+cu_summaries_prelim6 <- cu_summaries_prelim5[, names(cu_summaries_prelim5) %in% var_to_keep]
 
 ###############################################################################
 # Write to csv
 ###############################################################################
+cu_summaries <- cu_summaries_prelim6[, var_to_keep]
 
-write.csv(app1_display, 
+write.csv(cu_summaries, 
 					file = paste0("tables/full-CU-summary_", strftime(Sys.Date(), format = "%d%b%Y"), ".csv"), row.names = FALSE)

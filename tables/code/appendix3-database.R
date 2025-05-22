2eaa34776973bb2dea80211de1b5c7901c1c2817
Stephanie Peacock <stephanie.j.peacock@gmail.com>
2025-05-22 09:40:18 -0700

    Updated Appendix 3 tables to pull from biologicalstatus instead of app4 database view (https://salmonwatersheds.slack.com/archives/CKNVB4MCG/p1747877682233289?thread_ts=1747413816.520129&cid=CKNVB4MCG)

--- tables/code/appendix3-database.R
+++ tables/code/appendix3-database.R
@@ -4,7 +4,9 @@
 ###############################################################################
 
 library(dplyr)
-library(RPostgres)
+
+# Source functions to pull from database
+source("https://raw.githubusercontent.com/salmonwatersheds/population-indicators/refs/heads/master/code/functions_general.R")
 
 # Function to add commas if >= 10,000
 prettierNum <- function(x){
@@ -27,71 +29,44 @@ stripNA <- function(x){
 }
 
 ###############################################################################
-# Create connection to postreSQL salmonwatersheds database
-###############################################################################
-library(RPostgreSQL)
-
-dsn_database <- "salmondb_prod"   # Specify the name of your Database
-dsn_hostname <- "3.99.23.175"  # Specify host name e.g.:"aws-us-east-1-portal.4.dblayer.com"
-dsn_port <- "5432"                # Specify your port number. e.g. 98939
-dsn_uid <- "salmonwatersheds"         # Specify your username. e.g. "admin"
-dsn_pwd <- readline(prompt="Enter database password: " )    # Specify your password. e.g. "xxx"
-
-tryCatch({
-	drv <- RPostgres::Postgres()
-	print("Connecting to Database…")
-	connec <- dbConnect(drv, 
-											dbname = dsn_database,
-											host = dsn_hostname, 
-											port = dsn_port,
-											user = dsn_uid, 
-											password = dsn_pwd)
-	print("Database Connected!")
-},
-error=function(cond) {
-	print("Unable to connect to Database.")
-})
-
-###############################################################################
-# Import appendix4 view from appdata
+# Import relevant views from appdata
 ###############################################################################
 
-# Get app3 view
-app3 <- dbGetQuery(
-	conn = connec, 
-	statement = "SELECT * FROM appdata.vwdl_setr_appendix4"
-)
 
-app3$years_of_data <- as.numeric(app3$years_of_data)
+bs <- retrieve_data_from_PSF_databse_fun(name_dataset = "appdata.vwdl_biologicalstatus_output")
+
+# Filter out Yukon for now
+bs <- bs %>% filter(region != "Yukon")
+
 ###############################################################################
 # Make pretty for table display
 ###############################################################################
 
 app3_display <- data.frame(
-	Region = app3$region,
-	Species = app3$species_name,
-	Conservation.Unit = app3$cu_name_pse,
-	Current.Spawner.Abundance = prettierNum(app3$curr_spw),
-	Years = ifelse(is.na(app3$curr_spw), "", paste0(app3$curr_spw_start_year, "-", app3$curr_spw_end_year)),
-	Years.of.Data = stripNA(app3$years_of_data),
-	percentile_status = stripNA(app3$percentile_status),
-	percentile_red_prob = stripNA(app3$percentile_red_prob),
-	percentile_yellow_prob = stripNA(app3$percentile_yellow_prob),
-	percentile_green_prob = stripNA(app3$percentile_green_prob),
-	sr_status = stripNA(app3$sr_status),
-	sr_red_prob = ifelse(is.na(app3$sr_red_prob), " ", paste0(round(app3$sr_red_prob, 1), "%")),
-	sr_yellow_prob = ifelse(is.na(app3$sr_yellow_prob), " ", paste0(round(app3$sr_yellow_prob, 1), "%")),
-	sr_green_prob = ifelse(is.na(app3$sr_green_prob), " ", paste0(round(app3$sr_green_prob, 1), "%")),
-	WSP = stripNA(app3$wsp_status),
-	WSP.Year = stripNA(app3$wsp_status_yr),
-	COSEWIC = stripNA(app3$cosewic_status),
-	COSEWIC.Year = stripNA(app3$cosewic_status_yr),
-	Province = stripNA(app3$province_status),
-	Province.Year = stripNA(app3$province_status_yr),
-	S25 = ifelse(is.na(app3$`25%_spw`), " ", paste0(prettierNum(round(app3$`25%_spw`)), " (",  prettierNum(round(app3$`25%_spw_lower`)), " - ", prettierNum(round(app3$`25%_spw_upper`)), ")")),
-	S50 = ifelse(is.na(app3$`75%_spw`), " ", paste0(prettierNum(round(app3$`75%_spw`)), " (",  prettierNum(round(app3$`75%_spw_lower`)), " - ", prettierNum(round(app3$`75%_spw_upper`)), ")")),
-	Sgen = ifelse(is.na(app3$sgen), " ", paste0(prettierNum(round(app3$sgen)), " (",  prettierNum(round(app3$sgen_lower)), " - ", prettierNum(round(app3$sgen_upper)), ")")),
-	Smsy = ifelse(is.na(app3$smsy80), "", paste0(prettierNum(round(app3$smsy80)), " (",  prettierNum(round(app3$smsy80_lower)), " - ", prettierNum(round(app3$smsy80_upper)), ")"))
+	Region = bs$region,
+	Species = bs$species_name,
+	Conservation.Unit = bs$cu_name_pse,
+	Current.Spawner.Abundance = prettierNum(bs$curr_spw),
+	Years = ifelse(is.na(bs$curr_spw), "", paste0(bs$curr_spw_start_year, "-", bs$curr_spw_end_year)),
+	Years.of.Data = stripNA(bs$years_of_data),
+	percentile_status = stripNA(bs$percentile_status),
+	percentile_red_prob = stripNA(bs$percentile_red_prob),
+	percentile_yellow_prob = stripNA(bs$percentile_yellow_prob),
+	percentile_green_prob = stripNA(bs$percentile_green_prob),
+	sr_status = stripNA(bs$sr_status),
+	sr_red_prob = ifelse(is.na(bs$sr_red_prob), " ", paste0(round(bs$sr_red_prob, 1), "%")),
+	sr_yellow_prob = ifelse(is.na(bs$sr_yellow_prob), " ", paste0(round(bs$sr_yellow_prob, 1), "%")),
+	sr_green_prob = ifelse(is.na(bs$sr_green_prob), " ", paste0(round(bs$sr_green_prob, 1), "%")),
+	WSP = stripNA(bs$wsp_status),
+	WSP.Year = stripNA(bs$wsp_status_yr),
+	COSEWIC = stripNA(bs$cosewic_status),
+	COSEWIC.Year = stripNA(bs$cosewic_status_yr),
+	Province = stripNA(bs$province_status),
+	Province.Year = stripNA(bs$province_status_yr),
+	S25 = ifelse(is.na(bs$`25%_spw_lower`), " ", paste0(prettierNum(round(bs$`25%_spw`)), " (",  prettierNum(round(bs$`25%_spw_lower`)), " - ", prettierNum(round(bs$`25%_spw_upper`)), ")")),
+	S50 = ifelse(is.na(bs$`75%_spw`), " ", paste0(prettierNum(round(bs$`75%_spw`)), " (",  prettierNum(round(bs$`75%_spw_lower`)), " - ", prettierNum(round(bs$`75%_spw_upper`)), ")")),
+	Sgen = ifelse(is.na(bs$sgen), " ", paste0(prettierNum(round(bs$sgen)), " (",  prettierNum(round(bs$sgen_lower)), " - ", prettierNum(round(bs$sgen_upper)), ")")),
+	Smsy = ifelse(is.na(bs$smsy80), "", paste0(prettierNum(round(bs$smsy80)), " (",  prettierNum(round(bs$smsy80_lower)), " - ", prettierNum(round(bs$smsy80_upper)), ")"))
 )
 
 ###############################################################################
